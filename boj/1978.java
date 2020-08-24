c575a8ef5f9eecde16184dc75e7d88c7353311ef
hwlee3928 <hwlee3928@gmail.com>
2020-08-24 19:15:40 +0900

    [ADD] 소수 찾기

---
+++ boj/1978.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int size = Integer.parseInt(br.readLine());
+        int[] arr = new int[size];
+        int count = 0;
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < size; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < size; i++) {
+            if (prime(arr[i])) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+
+    public static boolean prime(int n) {
+
+        if (n < 2) {
+            return false;
+        }
+
+        for (int i = 2; i * i <= n; i++) {
+
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
