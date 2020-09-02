b06889e8856208c7d1589d3b03b2089cf3ad117e
hwlee3928 <hwlee3928@gmail.com>
2020-09-03 01:25:08 +0900

    N과 M (1)

--- boj/15649.java
+++ boj/15649.java
@@ -5,9 +5,8 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-
-    public static boolean[] check = new boolean[9];
-    public static int[] arr = new int[9];
+    private static boolean[] check = new boolean[9];
+    private static int[] arr = new int[9];
 
     public static void main(String[] args) throws IOException {
 
@@ -20,21 +19,17 @@ public class Main {
         recurse(0, n, m);
     }
 
-    public static void recurse(int idx, int n, int m) {
+    private static void recurse(int cnt, int n, int m) {
 
         // 1. 정답을 찾은 경우
         // 2. 불가능한 경우
-        if (idx == m) {
+        if (cnt >= m) {
             for (int i = 0; i < m; i++) {
-                System.out.print(arr[i]);
-                if (i != m - 1) {
-                    System.out.print(" ");
-                }
+                System.out.print(arr[i] + " ");
             }
             System.out.println();
             return;
         }
-
 
         // 3. 다음 경우 호출
         for (int i = 1; i <= n; i++) {
@@ -42,8 +37,8 @@ public class Main {
                 continue;
             }
             check[i] = true;
-            arr[idx] = i;
-            recurse(idx + 1, n, m);
+            arr[cnt] = i;
+            recurse(cnt + 1, n, m);
             check[i] = false;
         }
     }
