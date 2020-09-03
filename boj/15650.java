aadd5988b8f7508d7b55829e720828c1d7983037
hwlee3928 <hwlee3928@gmail.com>
2020-09-03 23:49:32 +0900

    N과 M (2)

--- boj/15650.java
+++ boj/15650.java
@@ -5,6 +5,51 @@ import java.util.StringTokenizer;
 
 public class Main {
 
+    public static int[] arr = new int[9];
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        recurse(1,0,n,m);
+    }
+
+    public static void recurse(int idx, int selected, int n, int m) {
+
+        // 1. 정답을 찾은 경우
+        if (selected == m) {
+            for (int i = 0; i < m; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+            return;
+        }
+        // 2. 불가능한 경우
+        if (idx > n) {
+            return;
+        }
+
+        // 3. 다음 경우 호출
+        arr[selected] = idx;
+        recurse(idx + 1, selected + 1, n, m);
+        arr[selected] = 0;
+        recurse(idx + 1, selected, n, m);
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
     public static boolean[] check = new boolean[9];
     public static int[] arr = new int[9];
 
