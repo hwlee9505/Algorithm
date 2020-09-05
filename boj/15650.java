114f8819e3aa83e5e1c83b2b857168e68a1af34d
hwlee3928 <hwlee3928@gmail.com>
2020-09-06 00:16:25 +0900

    N과 M (2)

--- boj/15650.java
+++ boj/15650.java
@@ -5,6 +5,59 @@ import java.util.StringTokenizer;
 
 public class Main {
 
+    public static boolean[] check = new boolean[9];
+    public static int[] arr = new int[9];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        System.out.println(recurse(0, 1, n, m));
+
+    }
+
+    // n과 m(1) + 오름차순 (int start 사용)
+    public static StringBuilder recurse(int cnt, int start, int n, int m) {
+
+        // 1. 정답인 경우
+        // 2. 불가능한 경우
+        if (cnt == m) {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < m; i++) {
+                sb.append(arr[i] + " ");
+            }
+            sb.append("\n");
+            return sb;
+        }
+
+        StringBuilder answer = new StringBuilder();
+
+        // 3. 다음 경우 호출
+        for (int i = start; i <= n; i++) {
+            if (check[i]) {
+                continue;
+            }
+            check[i] = true;
+            arr[cnt] = i;
+            answer.append(recurse(cnt + 1, i + 1, n, m));
+            check[i] = false;
+        }
+        return answer;
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////////////
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
     public static int[] arr = new int[9];
 
     public static void main(String[] args) throws IOException {
