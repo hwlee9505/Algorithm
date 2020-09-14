6f04352d6ecbccc97536ab86071227183ffc6d63
hwlee3928 <hwlee3928@gmail.com>
2020-09-14 22:27:42 +0900

    DFS와 BFS

--- boj/1260.java
+++ boj/1260.java
@@ -93,3 +93,99 @@ public class Main {
         }
     }
 }
+
+///////////////////////////////////////////////////////////////////////
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+class Pair {
+
+    int first;
+    int second;
+
+    public Pair(int first, int second) {
+        this.first = first;
+        this.second = second;
+    }
+}
+
+public class Main {
+    public static ArrayList<Integer>[] arr;
+    public static boolean[] check;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int start = Integer.parseInt(st.nextToken());
+
+        arr = new ArrayList[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            arr[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            arr[from].add(to);
+            arr[to].add(from);
+        }
+
+        for (int i = 1; i <= n; i++) {
+            Collections.sort(arr[i]);
+        }
+
+        check = new boolean[n + 1];
+        dfs(start);
+        System.out.println();
+        check = new boolean[n + 1];
+        bfs(start);
+
+    }
+
+    public static void dfs(int x) {
+        Stack<Pair> stack = new Stack<>();
+        stack.push(new Pair(x, 0));
+        check[x] = true;
+        System.out.print(x + " ");
+        while (!stack.isEmpty()) {
+            Pair p = stack.pop();
+            int node = p.first;
+            int start = p.second;
+            for (int i = start; i < arr[node].size(); i++) {
+                int next = arr[node].get(i);
+                if(check[next] == false){
+                    System.out.print(next + " ");
+                    check[next] = true;
+                    stack.push(new Pair(node, i + 1)) ;
+                    stack.push(new Pair(next, 0));
+                    break;
+                }
+            }
+        }
+    }
+
+    public static void bfs(int start) {
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(start);
+        check[start] = true;
+        while (!q.isEmpty()) {
+            int x = q.poll();
+            System.out.print(x + " ");
+            for (int i : arr[x]) {
+                if (check[i] == false) {
+                    check[i] = true;
+                    q.add(i);
+                }
+            }
+        }
+    }
+}
