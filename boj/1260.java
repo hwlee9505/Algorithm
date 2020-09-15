024a65bbc3b61af9fe62258d6640200d6e41f3ff
hwlee3928 <hwlee3928@gmail.com>
2020-09-15 13:47:58 +0900

    DFS와 BFS

--- boj/1260.java
+++ boj/1260.java
@@ -189,3 +189,128 @@ public class Main {
         }
     }
 }
+
+-------------------------------------------------------------------------------
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+class Edge implements Comparable<Edge> {
+
+    int from;
+    int to;
+
+    public Edge(int from, int to) {
+        this.from = from;
+        this.to = to;
+    }
+
+    // 그냥 별거없다. 정렬 기준 세우는 것이다.
+    @Override
+    public int compareTo(Edge that) {
+        if (this.from < that.from) {
+            return -1;
+        } else if (this.from == that.from) {
+            if (this.to < that.to) {
+                return -1;
+            } else if (this.to == that.to) {
+                return 0;
+            } else {
+                return 1;
+            }
+        } else {
+            return 1;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Edge{" +
+                "from=" + from +
+                ", to=" + to +
+                '}';
+    }
+}
+
+// 2. 간선 리스트 소스
+public class Main {
+
+    // 이 두가지 자료구조를 이용해서 간선 리스트를 만듬.
+    public static Edge[] edge;
+    public static int[] cnt;
+
+    public static boolean[] check;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int start = Integer.parseInt(st.nextToken());
+
+        edge = new Edge[m * 2];
+        cnt = new int[n + 1];
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            edge[i] = new Edge(from, to);
+            edge[m + i] = new Edge(to, from);
+        }
+
+        m *= 2;
+        Arrays.sort(edge);
+
+        for (int i = 0; i < m; i++) {
+            cnt[edge[i].from]++;        // 각 노드(vertex)의 차수 개수, from 의 개수 만큼 +
+        }
+
+        // 간선 리스트 특) 값이 축적 됨.
+        for (int i = 1; i <= n; i++) {
+            cnt[i] += cnt[i - 1];
+        }
+
+        check = new boolean[n + 1];
+        dfs(start);
+        System.out.println();
+        check = new boolean[n + 1];
+        bfs(start);
+
+
+    }
+
+    private static void dfs(int node) {
+        check[node] = true;
+        System.out.print(node + " ");
+        for (int i = cnt[node - 1]; i < cnt[node]; i++) {
+            int next = edge[i].to;
+            if (check[next] == false) {
+                dfs(next);
+            }
+        }
+    }
+
+    private static void bfs(int start) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(start);
+        check[start] = true;
+        while (!queue.isEmpty()) {
+            int node = queue.poll();
+            System.out.print(node + " ");
+            for (int i = cnt[node - 1]; i < cnt[node]; i++) {
+                int next = edge[i].to;
+                if (check[next] == false) {
+                    check[next] = true;
+                    queue.offer(next);
+                }
+            }
+        }
+    }
+}
