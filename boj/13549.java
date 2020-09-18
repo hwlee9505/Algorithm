54c9e68e5dd5f55a38b5e0bbfa82274ffbac2a3c
hwlee3928 <hwlee3928@gmail.com>
2020-09-19 00:33:52 +0900

    숨바꼭질 3

--- boj/13549.java
+++ boj/13549.java
@@ -66,3 +66,59 @@ public class Main {
 
 /////////////////////////////////////////////////////////////////////////////
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static final int MAX = 100001;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[] dist = new int[MAX];
+        boolean[] check = new boolean[MAX];
+
+        Queue<Integer> q1 = new LinkedList<>(); // 0 second
+        Queue<Integer> q2 = new LinkedList<>(); // 1 second
+
+        q1.offer(n);
+        check[n] = true;
+        dist[n] = 0;
+
+        while (!q1.isEmpty()) {
+            int now = q1.poll();
+
+            for (int next : new int[]{now*2, now-1, now+1}) {
+                if (next >= 0 && next < MAX) {
+                    if (check[next] == false) {
+                        check[next] = true;
+                        if (now*2 == next) {
+                            q1.add(next);
+                            dist[next] = dist[now];
+                        } else {
+                            q2.add(next);
+                            dist[next] = dist[now] + 1;
+                        }
+                    }
+                }
+            }
+
+            if (q1.isEmpty()) {
+                q1 = q2;
+                q2 = new LinkedList<>();
+            }
+        }
+
+        System.out.println(dist[k]);
+    }
+}
