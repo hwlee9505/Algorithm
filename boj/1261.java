2c2dbe4dd451741810333e5917d4cb38b8a997a6
hwlee3928 <hwlee3928@gmail.com>
2020-09-19 21:04:56 +0900

    알고스팟

--- boj/1261.java
+++ boj/1261.java
@@ -1,6 +1,85 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.StringTokenizer;
+
+class Pair {
+    int x;
+    int y;
+
+    public Pair(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+public class Main {
+
+    public static final int[] dx = {0, 0, 1, -1};
+    public static final int[] dy = {1, -1, 0, 0};
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int m = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+
+        int[][] arr = new int[n][m];
+
+        for (int i = 0; i < arr.length; i++) {
+            String temp = br.readLine();
+            for (int j = 0; j < arr[0].length; j++) {
+                arr[i][j] = temp.charAt(j) - '0';
+            }
+        }
+
+        int[][] dist = new int[n][m];
+        boolean[][] check = new boolean[n][m];
+
+        ArrayDeque<Pair> q = new ArrayDeque<>();
+
+        q.offer(new Pair(0, 0));
+        dist[0][0] = 0;
+        check[0][0] = true;
+
+        while (!q.isEmpty()) {
+
+            Pair priorPair = q.poll();
+
+            for (int i = 0; i < 4; i++) {
+                int nx = priorPair.x + dx[i];
+                int ny = priorPair.y + dy[i];
+
+                if ((0 <= nx && nx < n) && (0 <= ny && ny < m)) {
+                    if (check[nx][ny] == false) {
+                        if (arr[nx][ny] == 0) {
+                            q.offerFirst(new Pair(nx, ny));
+                            dist[nx][ny] = dist[priorPair.x][priorPair.y];
+                            check[nx][ny] = true;
+                        }
+
+                        if (arr[nx][ny] == 1) {
+                            q.offerLast(new Pair(nx, ny));
+                            dist[nx][ny] = dist[priorPair.x][priorPair.y] + 1;
+                            check[nx][ny] = true;
+                        }
+                    }
+                }
+            }
+        }
+
+        System.out.println(dist[n - 1][m - 1]);
+    }
+}
+
+///////////////////////////////////////////////////////////////////////
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
