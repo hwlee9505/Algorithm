e6b850212ebf8cfb11f5445c30234e5984e45dff
hwlee3928 <hwlee3928@gmail.com>
2020-09-21 17:58:44 +0900

    1로 만들기 x

--- boj/1463.java
+++ boj/1463.java
@@ -51,3 +51,37 @@ public class Main {
         }
     }
 }
+
+/////////////////////////////////////////////////////////////////////////////
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+// 작은 문재 -> 큰 문제
+
+public class Main {
+
+    public static int[] memo;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        memo = new int[n + 1];
+
+        memo[1] = 0;
+
+        for (int i = 2; i <= n; i++) {
+            memo[i] = memo[i - 1] + 1;
+            if (i % 2 == 0 && memo[i] > memo[i / 2] + 1) {
+                memo[i] = memo[i / 2] + 1;
+            }
+            if (i % 3 == 0 && memo[i] > memo[i / 3] + 1) {
+                memo[i] = memo[i / 3] + 1;
+            }
+        }
+        System.out.println(memo[n]);
+    }
+}
