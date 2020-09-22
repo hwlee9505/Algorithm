7c40bf37e29ae696e85ba222b3312ed1af0276f6
hwlee3928 <hwlee3928@gmail.com>
2020-09-22 14:42:29 +0900

    1,2,3 더하기

--- boj/9095.java
+++ boj/9095.java
@@ -2,6 +2,80 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+// Dynamic Programming [Overlapping SubProblem(부분 문제가 겹친다), Optimal Substructure(문제의 정답을 작은 문제의 정답에서 찾을 수 있다)]
+// 1. Top Down
+//      1) 점화식 구하기
+//      2) 문제 -> 작은 문제
+//
+// DP는 최적의 경우를 알아내기 위해 사용한다 들었는데 이 문제를 DP로?
+//
+// D[N] = 정수 N을 1,2,3의 합으로 나타내는 방법의 수를 구하는 문제
+
+// 점화식 경우를 왜인지 생각해봐
+// D[N] = D[N-1] + D[N-2] + D[N-3]
+
+public class Main {
+
+    public static int[] memo;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+
+            // 1. top down
+            int temp = Integer.parseInt(br.readLine());
+            memo = new int[temp + 1];
+            System.out.println(topDown(temp));
+
+            // 2. bottom up
+            memo = new int[temp + 1];
+            System.out.println(bottomUp(temp));
+
+        }
+    }
+
+    public static int topDown(int n) {
+
+        // 0만드는 경우는
+        // 아무것도 선택하지 않은 경우 == 1
+        // 공집합도 1로 일반화 해주면 좋다.
+        if (n == 0 || n == 1) {
+            return 1;
+        } else if (n == 2) {
+            return 2;
+        } else {
+
+            if (memo[n] > 0) {
+                return memo[n];
+            }
+            memo[n] = topDown(n - 1) + topDown(n - 2) + topDown(n - 3);
+        }
+        return memo[n];
+    }
+
+    public static int bottomUp(int n) {
+
+        memo[0] = 1;
+        memo[1] = 1;
+        memo[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            memo[n] = topDown(n-1) + topDown(n-2) + topDown(n-3);
+        }
+
+        return memo[n];
+    }
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
 
     public static int cnt;
