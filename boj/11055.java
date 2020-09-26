f07b9f615a0ce4e07c01a1b64fe4ce4b84744005
hwlee3928 <hwlee3928@gmail.com>
2020-09-26 20:31:59 +0900

    가장 큰 증가하는 부분 수열

---
+++ boj/11055.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static int[] arr;
+    public static int[] memo;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        arr = new int[n];
+        memo = new int[n];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        bottomUp(n);
+    }
+
+    public static void bottomUp(int n) {
+
+        for (int i = 0; i < n; i++) {
+            memo[i] = arr[i];
+            for (int j = 0; j < i; j++) {
+                if (arr[i] > arr[j] && memo[i] < memo[j] + arr[i]) {
+                    memo[i] = memo[j] + arr[i];
+                }
+            }
+        }
+
+        Arrays.sort(memo);
+        System.out.println(memo[n - 1]);
+    }
+}
