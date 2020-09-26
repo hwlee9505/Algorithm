7f60a3b2402951577d4bad228d381610aa82156d
hwlee3928 <hwlee3928@gmail.com>
2020-09-26 15:18:03 +0900

    포도주 시식

---
+++ boj/2156.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+//-------------------------------------
+// Dynamic Programming
+//
+// D[i][j] = arr[1], ..., arr[i] 까지 포도주가 있을 때 , 마실 수 있는 포도주의 최대의 양, arr[i]는 j번 연속해서 마신 포도주임
+//
+// 1. D[i][0] = 0번 연속해서 마신 포도주 -> arr[i]를 마시지 않음
+// 2. D[i][1] = 1번 연속해서 마신 포도주 -> arr[i-1]를 마시지 않음
+// 3. D[i][2] = 2번 연속해서 마신 포도주 -> arr[i-1]를 마시고, arr[i-2]는 마시지 않았어야 함
+public class Main {
+
+    public static int[] arr;
+    public static long[][] memo;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        arr = new int[n + 1];
+
+        for (int i = 1; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        memo = new long[n + 1][3];
+        System.out.println(bottomUp(n));
+    }
+
+    public static long bottomUp(int n) {
+
+        memo[1][0] = 0;
+        memo[1][1] = arr[1];
+        memo[1][2] = arr[1];
+
+        for (int i = 2; i <= n; i++) {
+            // i번째 X
+            memo[i][0] = Math.max(memo[i - 1][0], Math.max(memo[i - 1][1], memo[i - 1][2]));
+            // i번째 마심 - 1번 연속
+            memo[i][1] = memo[i - 1][0] + arr[i];
+            // i번째 마심 - 2번 연속
+            memo[i][2] = memo[i - 1][1] + arr[i];
+        }
+        return Math.max(memo[n][0], Math.max(memo[n][1], memo[n][2]));
+    }
+}
