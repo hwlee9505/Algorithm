0d5f4ced5b1cc46a29cf7587ccdadb5e5efcc357
hwlee3928 <hwlee3928@gmail.com>
2020-10-04 15:07:30 +0900

    N과 M (1)

--- boj/15649.java
+++ boj/15649.java
@@ -43,3 +43,56 @@ public class Main {
         }
     }
 }
+
+/////////////////////////////////////////////////////////////////////////
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static ArrayList<Integer> list = new ArrayList<>();
+    public static boolean[] check;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        check = new boolean[n + 1];
+
+        recurse(n, m, 0);
+
+    }
+
+    public static void recurse(int n, int m, int idx) {
+
+        // 1. 정답인 경우
+        if (idx == m) {
+            for (int i : list) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        // 2. 불가능한 경우
+
+        // 3. 다음경우 호출
+        for (int i = 1; i <= n; i++) {
+            if (check[i] == false) {
+                check[i] = true;
+                list.add(i);
+                recurse(n, m, idx + 1);
+                list.remove(list.size() - 1);
+                check[i] = false;
+            }
+        }
+    }
+}
